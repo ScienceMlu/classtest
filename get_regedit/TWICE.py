c31d7496250e496ba043867c8db4914cc7c35436
ScienceMlu <z1981241454@gmail.com>
2018-06-12 23:21:47 +0800

    Regedit check

--- get_regedit/TWICE.py
+++ get_regedit/TWICE.py
@@ -1,42 +1,20 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
 import winreg
-"""
-key = "SYSTEM\CurrentControlSet\Control\Print\Printers\Microsoft Print to PDF"
-open_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key)
-countkey = winreg.QueryInfoKey(open_key)[0]
-keylist = []
-for i in range(int(countkey)):
-    name = winreg.EnumKey(open_key, i)  # 获取子键名
-    keylist.append(name)
-winreg.CloseKey(open_key)
-print(keylist)
-"""
-import winreg
+class regedit:
+    def __init__(self, path, mainfile):
+        self.path = path
+        self.mainfile = mainfile
+    def print_value(self):
+        key = winreg.OpenKey(self.mainfile, self.path)
+        countkey = winreg.QueryInfoKey(key)[1]
+        for i in range(int(countkey)):
+            name = winreg.EnumValue(key, i)  # 获取数据，【0】：名称 【1】类型 【2】：数值
+            value = winreg.QueryValueEx(key, name[0])
+            print('名称：'+str(name[0])+','+'数据：'+ str(value[0]))
 
-key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SYSTEM\CurrentControlSet\Control\Print\Printers\Microsoft Print to PDF\PrinterDriverData")
+# path = r"SYSTEM\CurrentControlSet\Control\Print\Printers\Microsoft Print to PDF\PrinterDriverData"
+#  mainfile = winreg.HKEY_LOCAL_MACHINE
 
-"""
-# 获取该键的所有键值，因为没有方法可以获取键值的个数，所以只能用这种方法进行遍历
-try:
-    i = 0
-    while 1:
-        # EnumValue方法用来枚举键值，EnumKey用来枚举子键
-        name, value, type = winreg.EnumValue(key, i)
-    print(repr(name))
-    i += 1
-except WindowsError:
-    print('no')
-
-# 如果知道键的名称，也可以直接取值
-"""
-countkey = winreg.QueryInfoKey(key)[1]
-print(winreg.QueryInfoKey(key))
-keylist = []
-valuelist = []
-for i in range(int(countkey)):
-    value = winreg.EnumValue(key, i)  # 获取数据，【0】：名称 【1】类型 【2】：数值
-    print('名称：%s, 数据：%' %(value[0], value[1]))
-    #keylist.append(value[0])
-    #valuelist.append(value[2])
-#print(keylist)
-
-
+regedit1 =regedit(path=r"SYSTEM\CurrentControlSet\Control\Print\Printers\Microsoft Print to PDF\PrinterDriverData", mainfile=winreg.HKEY_LOCAL_MACHINE)
+regedit1.print_value()
